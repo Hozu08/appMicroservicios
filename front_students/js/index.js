a2e98196dd348efea8c62091679acb02259394eb
Hozu08 <luicastillo@uniboyaca.edu.co>
2023-06-02 20:15:24 -0500

    Version 11 - no se inserta una bandera para agregar/modificar, en cambio se anida el boton #acceptMA, se tiene un error al mostrar los estudiantes registrados

--- front_students/js/index.js
+++ front_students/js/index.js
@@ -1,12 +1,13 @@
+
+    let addStudent = () => {};
+    let start = () => {};
+    let modifyW = () => {};
+    let deleteW = () => {};
+
 //Carga las funciones y demas una vez se a cargado completamente el HTML
 
-let addStudent = () => {};
-let start = () => {};
-let modifyW = () => {};
-let deleteW = () => {};
-
 $(document).ready(start = () => {
-
+    
     //Trae los estudiantes en la base de datos
 
     $.ajax({
@@ -18,17 +19,28 @@ $(document).ready(start = () => {
         let iHtml = '';
 
         students.forEach(student => {
-            iHtml += '<tr>';
-            iHtml +=    '<td>' + student.codigo + '</td>';
-            iHtml +=    '<td>' + student.nombres + '</td>';
-            iHtml +=    '<td>' + student.apellidos + '</td>';
-            iHtml +=    '<td><button onclick="scoreW(' + student.codigo + ')" type="button">Notas</button></td>';
-            iHtml +=    '<td><button onclick="modifyW(' + student.codigo + ')" type="button">Modificar</button></td>';
-            iHtml +=    '<td><button onclick="deleteW(' + student.codigo + ')" type="button">Eliminar</button></td>';
-            iHtml += '</tr>';
+            iHtml += '<thead>';
+            iHtml +=    '<tr>';
+            iHtml +=        '<th>Código</th>';
+            iHtml +=        '<th>Nombre</th>';
+            iHtml +=        '<th></th>';
+            iHtml +=        '<th></th>';
+            iHtml +=        '<th></th>';
+            iHtml +=    '</tr>';
+            iHtml += '</thead>';
+            iHtml += '<tbody>';
+            iHtml +=    '<tr>';
+            iHtml +=        '<td>' + student.codigo + '</td>';
+            iHtml +=        '<td>' + student.nombres + '</td>';
+            iHtml +=        '<td>' + student.apellidos + '</td>';
+            iHtml +=        '<td><button onclick="scoreW(' + student.codigo + ')" type="button">Notas</button></td>';
+            iHtml +=        '<td><button onclick="modifyW(' + student.codigo + ')" type="button">Modificar</button></td>';
+            iHtml +=        '<td><button onclick="deleteW(' + student.codigo + ')" type="button">Eliminar</button></td>';
+            iHtml +=    '</tr>';
+            iHtml += '</tbody>'
         });
 
-        $('#containerST').append(iHtml);
+        $('#containerST').html(iHtml);
 
     }).fail((error) => {
         console.error(error);
@@ -37,55 +49,48 @@ $(document).ready(start = () => {
     /*Permite ocultar o mostrar las ventanas de tabla estudiantes o agregar/modificar 
     estudiante, ademas asigna el titulo y nombre de boton*/
 
-    $('#agregarEBtn').click(() => {
-        document.getElementById('containerMA').setAttribute('style', 'visibility:visible');
-        document.getElementById('containerMain').setAttribute('style', 'visibility:hidden');
-        $('#title').text('Agregar estudiante');
-        $('#acceptMA').text('Agregar');
-    });
-
-
     $('#closeMABtn').click(() => {
         document.getElementById('containerMA').setAttribute('style', 'visibility:hidden');
         document.getElementById('containerMain').setAttribute('style', 'visibility:visible');
     });
 
-    $('#acceptMA').click(() => {
-        document.getElementById('containerMA').setAttribute('style', 'visibility:hidden');
-        document.getElementById('containerMain').setAttribute('style', 'visibility:visible');
-        start();
+    $('#agregarEBtn').click(() => {
+        document.getElementById('containerMA').setAttribute('style', 'visibility:visible');
+        document.getElementById('containerMain').setAttribute('style', 'visibility:hidden');
+        $('#title').text('Agregar estudiante');
+        $('#acceptMA').text('Agregar');
+        $('#acceptMA').click(addStudent = () => {
+            document.getElementById('containerMA').setAttribute('style', 'visibility:hidden');
+            document.getElementById('containerMain').setAttribute('style', 'visibility:visible');
+            
+            $code = $('#codeMA').val();
+            $name = $('#nameMA').val();
+            $lastname = $('#lastnameMA').val();    
+            $.ajax({
+                url: 'http://localhost:8000/estudiante',
+                method: 'post',
+                data:{
+                    codigo: $code,
+                    nombres: $name,
+                    apellidos: $lastname
+                }
+            }).done(response=>{
+                const dataJson = JSON.parse(response);
+                const msg = dataJson.data; 
+                start();
+            })
+        });    
     });
-
-    //Agregar estudiante
-
-    $('#acceptMA').click(addStudent = () => {
-        $code = $('#codeMA').val();
-        $name = $('#nameMA').val();
-        $lastname = $('#lastnameMA').val();    
-        $.ajax({
-            url: 'http://localhost:8000/estudiante',
-            method: 'post',
-            data:{
-                codigo: $code,
-                nombres: $name,
-                apellidos: $lastname
-            }
-        }).done(response=>{
-            const dataJson = JSON.parse(response);
-            const msg = dataJson.data; 
-            alert(msg);
-        })
-    });    
-
-    //Modificar estudiante
 
     modifyW = ($code) => {
         document.getElementById('containerMA').setAttribute('style', 'visibility:visible');
         document.getElementById('containerMain').setAttribute('style', 'visibility:hidden');
         $('#title').text('Modificar estudiante');
         $('#acceptMA').text('Modificar');
-
         $('#acceptMA').click(() => {
+            document.getElementById('containerMA').setAttribute('style', 'visibility:hidden');
+            document.getElementById('containerMain').setAttribute('style', 'visibility:visible');
+
             $codeM = $('#codeMA').val();
             $name = $('#nameMA').val();
             $lastname = $('#lastnameMA').val();
@@ -100,8 +105,9 @@ $(document).ready(start = () => {
             }).done(response=>{
                 const dataJson = JSON.parse(response);
                 const msg = dataJson.data; 
+                start();
             })    
-        });   
+        });    
     }
 
     //Eliminar estudiante
@@ -113,6 +119,7 @@ $(document).ready(start = () => {
         }).done(response=>{
             const dataJson = JSON.parse(response);
             const msg = dataJson.data; 
+            start();
         });     
     }
 });
